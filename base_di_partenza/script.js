01561058110859632837d032ef415c27be225657
mary1457 <piabaldoin@gmail.com>
2024-07-12 16:56:04 +0200

    cambio colore bottone

--- base_di_partenza/script.js
+++ base_di_partenza/script.js
@@ -1,13 +1,19 @@
 const header =document.getElementById("header");
 const banner=document.getElementById("banner-container");
+const bottone=document.getElementById("bottone");
 window.addEventListener("scroll", function() {
     if (window.pageYOffset>banner.offsetHeight) {
       
         if (window.pageYOffset>header.offsetHeight)
-        {banner.style.backgroundColor="white";}
-        else { banner.style.backgroundColor="";}
+        {banner.style.backgroundColor="white";
+            bottone.style.backgroundColor="green";
+        }
+        else { banner.style.backgroundColor="";
+            bottone.style.backgroundColor="";
+        }
     }
     else  {
         banner.style.backgroundColor="";
+        bottone.style.backgroundColor="";
     }
 })
